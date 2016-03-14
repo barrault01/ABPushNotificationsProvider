bda22ccd8ca6ed6884a22f84483381c9792d3717
Antoine Barrault <barrault01@gmail.com>
2016-03-14 10:33:39 -0300

    make basic request

--- ABPushNotificationsProvider/PerfectHandlers.swift
+++ ABPushNotificationsProvider/PerfectHandlers.swift
@@ -7,6 +7,12 @@
 //
 
 import PerfectLib
+
+let AUTH_DB_PATH = PerfectServer.staticPerfectServer.homeDir() + serverSQLiteDBs + "AuthenticatorDb"
+
+// HTTP authentication realm
+let AUTH_REALM = "Authenticator Perfect Example"
+
 //public method that is being called by the server framework to initialise your module.
 public func PerfectServerModuleInit() {
     
@@ -15,18 +21,92 @@ public func PerfectServerModuleInit() {
     Routing.Handler.registerGlobally()
     
     // Create Routes
-    Routing.Routes["GET", ["/", "index.html"] ] = { (_:WebResponse) in return IndexHandler() }
+    Routing.Routes["GET", ["/tokens", "index.html"] ] = { (_:WebResponse) in return IndexHandler() }
+    Routing.Routes["POST", "/token"] = { _ in return Echo3Handler() }
+
     
     // Check the console to see the logical structure of what was installed.
     print("\(Routing.Routes.description)")
+    
+    // For example, demo purposes - remove the existing database so that one has to register each time
+    // Comment this little section out if you want the database to persist across runs.
+    let oldFile = File(AUTH_DB_PATH)
+    if oldFile.exists() {
+        oldFile.delete()
+    }
+    
+    // Create our SQLite tracking database.
+    do {
+        let sqlite = try SQLite(AUTH_DB_PATH)
+        try sqlite.execute("CREATE TABLE IF NOT EXISTS tokens (id_token INTEGER, key TEXT)")
+    } catch {
+        print("Failure creating tracker database at " + AUTH_DB_PATH)
+    }
+
 }
+
+
 
 //Create a handler for index Route
 class IndexHandler: RequestHandler {
     
     func handleRequest(request: WebRequest, response: WebResponse) {
         response.appendBodyString("Hello World")
+        try! self.getAllTokens(response)
         response.requestCompletedCallback()
     }
+    
+    func getAllTokens (response: WebResponse) throws {
+        
+        // Try to get the last tap instance from the database
+        let sqlite = try SQLite(AUTH_DB_PATH)
+        defer {
+            sqlite.close()
+        }
+        
+        var values = [String]()
+        
+        try sqlite.forEachRow("SELECT key FROM tokens") {
+            (stmt:SQLiteStmt, i:Int) -> () in
+            
+            let key = stmt.columnText(0)
+            values.append(key)
+            response.appendBodyString("\n \(key)")
+
+        }
+        
+    }
+
 }
+
+
+class Echo3Handler: RequestHandler {
+    
+    func handleRequest(request: WebRequest, response: WebResponse) {
+        response.appendBodyString("<html><body>Raw POST handler: You POSTED to path \(request.requestURI()) with content-type \(request.contentType()) and POST body \(request.postBodyString)</body></html>")
+        
+        if let param = request.param("id") {
+            try! createToken(param)
+        }
+
+        response.requestCompletedCallback()
+    }
+
+    
+    func createToken(token : String) throws {
+        
+        let sqlite = try SQLite(AUTH_DB_PATH)
+        defer {
+            sqlite.close()
+        }
+
+        try sqlite.execute("INSERT INTO tokens (key) VALUES (?)", doBindings: {
+            (stmt:SQLiteStmt) -> () in
+            
+            try stmt.bind(1, token)
+        })
+
+    }
+}
+
 
